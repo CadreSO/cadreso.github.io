6ed0a22d0e1e4692933d60b52eab6924a1cf89f9
Angelabelle Abarientos <ab.abarientos@gmail.com>
2020-02-07 16:30:31 -0800

    slideshow added

---
+++ javascript/slideShow.js
@@ -0,0 +1,26 @@
+var myIndex = 0;
+
+
+function slideShow() {
+    var x = document.getElementsByClassName("mySlides");
+    console.log(x.length);
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    if (x.length > 0) {
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex - 1].style.display = "block";
+    }
+    else
+    {
+
+    }
+    setTimeout(slideShow, 2000);
+    // Change image every 2 seconds
+}
+
+slideShow();
